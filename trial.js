a22d18f2f0d7c2d4760058c386ca54193bd809c9
ririErliza <ryantiadam@yahoo.co.uk>
2021-12-06 11:19:14 +0100

    Practice Solo Exercise

---
+++ trial.js
@@ -0,0 +1,56 @@
+//let random = Math.floor (Math.random () * 21);
+//console.log(random);
+
+/*let me = {
+    name : "Erliza",
+    surname : "Erbaryanti",
+    age : 32,
+
+};
+
+console.log(me);
+
+delete me.age;
+
+me.skills = [html, css, javascript]
+
+me.pop () ;
+
+
+
+
+const dice = function (n){
+    const arr = [];
+    for (let i = 0 ; i<=n; i++)
+{
+arr.push(Math.floor (Math.random()*(6-1));
+}
+return arr;
+};
+console.log(dice(6));
+
+
+
+
+
+wholsBigger = findMax(3,8);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+
+console.log(wholsBigger);
+
+*/
+
+
+let splitMe = "Don't give up"
+const myArray = splitMe.split (" ")
+
+console.log(myArray);
